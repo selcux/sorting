151ab63c55613365022d64a7ebb2234fabd09c76
Selçuk Öztürk <selcukahmed@gmail.com>
2016-11-13 23:41:28 +0300

    Bubble sort implemented.

--- lib/bubble_sort.ex
+++ lib/bubble_sort.ex
@@ -1,4 +1,28 @@
 defmodule BubbleSort do
   @moduledoc false
+
+  def sort(list) when length(list) <= 1, do: list
+
+  def sort(list) do
+    do_sort(list, [])
+  end
   
+  defp do_sort([x], acc), do: [x | acc]
+
+  defp do_sort(list, acc) do
+    {unsorted, [last]} =
+    shift(list)
+    |> Enum.split(-1)
+
+    do_sort(unsorted, [last | acc])
+  end
+
+  defp shift([x]), do: [x]
+  
+  defp shift([x, y | t]) do
+    if (x > y),
+        do: [y | shift([x | t])],
+        else: [x | shift([y | t])]
+  end
+
 end
--- test/bubble_sort_test.exs
+++ test/bubble_sort_test.exs
@@ -1,4 +1,31 @@
 defmodule BubbleSortTest do
-  @moduledoc false
-  
+  use ExUnit.Case
+  doctest BubbleSort
+
+  test "empty" do
+    assert BubbleSort.sort([]) == []
+  end
+
+  test "single element" do
+    assert BubbleSort.sort([0]) == [0]
+  end
+
+  test "multiple elements" do
+    assert BubbleSort.sort([1, 2]) == [1, 2]
+    assert BubbleSort.sort([2, 1]) == [1, 2]
+  end
+
+  test "nonunique elements" do
+    assert BubbleSort.sort([1, 1]) == [1, 1]
+    assert BubbleSort.sort([1, 1, 2]) == [1, 1, 2]
+    assert BubbleSort.sort([1, 2, 1]) == [1, 1, 2]
+    assert BubbleSort.sort([2, 1, 1]) == [1, 1, 2]
+  end
+
+  test "negative elements" do
+    assert BubbleSort.sort([-1, 0, 1]) == [-1, 0, 1]
+    assert BubbleSort.sort([-1, -1, 1]) == [-1, -1, 1]
+    assert BubbleSort.sort([-1, -2, 1]) == [-2, -1, 1]
+    assert BubbleSort.sort([1, -2, 0]) == [-2, 0, 1]
+  end
 end
